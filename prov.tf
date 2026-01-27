0ac22107c7bda4764904b599be1339edaa66e97b
naroonboy <77798565+naroonboy@users.noreply.github.com>
2026-01-27 18:40:12 +0530

    Create prov.tf

---
+++ prov.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    signalfx = {
+      source  = "splunk-terraform/signalfx"
+      version = "~> 9.0"
+    }
+  }
+}
+
+provider "signalfx" {
+  auth_token = var.splunk_access_token
+  realm      = var.splunk_realm
+}
